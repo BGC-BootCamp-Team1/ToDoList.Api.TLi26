1ac4f5362cc827e9bd50777ad97e3dc04fb2060f
Ting Li <TLi26@slb.com>
2024-11-05 02:45:02 +0800

    feat: add ExceptionFilter

---
+++ ToDoList.Api/ExceptionFilter/CustomExceptionFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using ToDoList.Core.ApplicationExcepetions;
+
+namespace ToDoList.Api.ExceptionFilter
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is ExceedMaxModificationException ||
+                context.Exception is ExceedMaxTodoItemsPerDueDateException ||
+                context.Exception is InvalidDueDateSettingOptionException ||
+                context.Exception is NoAvailableDaysException ||
+                context.Exception is TooEarlyDueDateException
+                )
+            {
+                context.Result = new ObjectResult(context.Exception.Message)
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+            else 
+            {
+                context.Result = new ObjectResult("Unknow Error")
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+            context.ExceptionHandled = true;
+        }
+    }
+}
