c9844e1b57311509c57dae139e23a906bed4c088
Anyan Zhao <AZhao4@slb.com>
2024-11-04 16:39:17 +0800

    test: add Create test

---
+++ ToDoList.Api.ApiTests/CreateOneTodoItemTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ToDoList.Api.Models;
+
+namespace ToDoList.Api.ApiTests
+{
+    public class CreateOneTodoItemTest : IClassFixture<WebApplicationFactory<Program>>, IAsyncLifetime
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+        private IMongoCollection<ToDoItem> _mongoCollection;
+
+        public CreateOneTodoItemTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var mongoDatabase = mongoClient.GetDatabase("TodoItem");
+            _mongoCollection = mongoDatabase.GetCollection<ToDoItem>("todos");
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _mongoCollection.DeleteManyAsync(FilterDefinition<ToDoItem>.Empty);
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        [Fact]
+        public async void should_create_todo_item()
+        {
+            var todoItemRequst = new ToDoItemCreateRequest()
+            {
+                Description = "test create",
+                Done = false,
+                Favorite = true,
+            };
+
+            var json = JsonSerializer.Serialize(todoItemRequst);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("/api/v1/todoitems", content);
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+    }
+}
